a1cee4c9c8ca27d16244644b1031bfaa240e76f0
Logan <logansummers2700@gmail.com>
2025-11-16 17:55:54 -0500

    Add computerChoice Function
    Function returns the RPS choice for the computer

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    let randInt = Math.random();
+    let computerChoice = null;
+
+    if (randInt <= 0.33) {
+        computerChoice = "Rock";
+    }
+    else if (randInt >= 0.331 && randInt <= 0.66){
+        computerChoice = "Paper";
+    }
+    else if (randInt >= 0.661){
+        computerChoice = "Scissors";
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
